2ed6fa1f2a5172782f19afa2a2815802bffec67a
MattiaDallaCosta <matty7787878@gmail.com>
2024-04-29 15:51:50 +0200

    (not tested) added touch interrupt from touch example

--- progetto_cmls/progetto_cmls.ino
+++ progetto_cmls/progetto_cmls.ino
@@ -1,19 +1,23 @@
 #include <Arduino.h>
 #include <BLEMidi.h>
+#include <Wire.h>
 
 // #define LED 4
 
 //the b-frame is the frame of the body while the e-frame is the frame of the environament
 
 typedef struct {
-  float_t pos[3], vel[3];
-  float_t dir_mat[9];                                         // rotation matrix of the b-frame in relation to the e-frame initialized as [[1 0 0],[0 1 0],[0 0 1]]
+  float_t pos[3] = {0, 0, 0}, vel[3] = {0, 0, 0};
+  float_t dir_mat[9] = {1, 0, 0, 0, 1, 0, 0, 0, 1};                                         // rotation matrix of the b-frame in relation to the e-frame initialized as [[1 0 0],[0 1 0],[0 0 1]]
 } state_t;                                                    // state containing the current position velocity and orientation vectors
 
-float_t acel[6], gyro[6], init_g[3];                          // sensor data
-uint8_t curr = 0;                                             // index to deduce the current state (treated as boolean)
-uint32_t time[2];
+float_t acel[3] = {0, 0, 0}, gyro[3] = {0, 0, 0}, init_g[3] = {0, 0, 0};                          // sensor data
+bool curr = 0;                                             // index to deduce the current state (treated as boolean)
+uint32_t t[2];
 state_t state[2];                                             // current and previous state (used for calculations)
+
+bool test_low = 1, c_t = 0, touch[2] = {0, 0};
+uint32_t threshold = 55;
 
 // ---- MATRIX OPERATIONS ----
 
@@ -33,22 +37,22 @@ void rotm2Eul(float_t rotm[9], float_t eul[3]){
   // TODO
 }
 
-void matMul(float_t out[9], float_t delta[9], float_t old[9]) { // matrix matrix multiplication
-  out[0] = delta[0]*old[0] + delta[1]*old[3] + delta[2]*old[6];
-  out[1] = delta[0]*old[1] + delta[1]*old[4] + delta[2]*old[7];
-  out[2] = delta[0]*old[2] + delta[1]*old[5] + delta[2]*old[8];
-  out[3] = delta[3]*old[0] + delta[4]*old[3] + delta[5]*old[6];
-  out[4] = delta[3]*old[1] + delta[4]*old[4] + delta[5]*old[7];
-  out[5] = delta[3]*old[2] + delta[4]*old[5] + delta[5]*old[8];
-  out[6] = delta[6]*old[0] + delta[7]*old[3] + delta[8]*old[6];
-  out[7] = delta[6]*old[1] + delta[7]*old[4] + delta[8]*old[7];
-  out[8] = delta[6]*old[2] + delta[7]*old[5] + delta[8]*old[8];
+void matMul(float_t out[9], float_t delta[9], float_t old_mat[9]) { // matrix matrix multiplication
+  out[0] = delta[0]*old_mat[0] + delta[1]*old_mat[3] + delta[2]*old_mat[6];
+  out[1] = delta[0]*old_mat[1] + delta[1]*old_mat[4] + delta[2]*old_mat[7];
+  out[2] = delta[0]*old_mat[2] + delta[1]*old_mat[5] + delta[2]*old_mat[8];
+  out[3] = delta[3]*old_mat[0] + delta[4]*old_mat[3] + delta[5]*old_mat[6];
+  out[4] = delta[3]*old_mat[1] + delta[4]*old_mat[4] + delta[5]*old_mat[7];
+  out[5] = delta[3]*old_mat[2] + delta[4]*old_mat[5] + delta[5]*old_mat[8];
+  out[6] = delta[6]*old_mat[0] + delta[7]*old_mat[3] + delta[8]*old_mat[6];
+  out[7] = delta[6]*old_mat[1] + delta[7]*old_mat[4] + delta[8]*old_mat[7];
+  out[8] = delta[6]*old_mat[2] + delta[7]*old_mat[5] + delta[8]*old_mat[8];
 }
 
 void matVecMul(float_t out[3], float_t mat[9], float vec[3]) {            // matrix vector multiplication
-  old[0] = mat[0]*vec[0] + mat[1]*vec[1] + mat[2]*vec[2];
-  old[1] = mat[3]*vec[0] + mat[4]*vec[1] + mat[5]*vec[2];
-  old[2] = mat[6]*vec[0] + mat[7]*vec[1] + mat[8]*vec[2];
+  out[0] = mat[0]*vec[0] + mat[1]*vec[1] + mat[2]*vec[2];
+  out[1] = mat[3]*vec[0] + mat[4]*vec[1] + mat[5]*vec[2];
+  out[2] = mat[6]*vec[0] + mat[7]*vec[1] + mat[8]*vec[2];
 }
 
 void matTrans(float_t out[3], float_t in[3]) {                            // transposes the in matrix in the out matrix (since rotation matrices are orthogonal it is equal to inverting it)
@@ -65,15 +69,6 @@ void matTrans(float_t out[3], float_t in[3]) {                            // tra
 
 // ---- PHYSICAL OPERATIONS ----
 
-void stateInit(state_t s) {                                               // initializes the state variables for the given state_t variable
-   memset(s.pos, 0, 3*sizeof(float_t));
-   memset(s.vel, 0, 3*sizeof(float_t));
-   memset(s.dir_mat, 0, 9*sizeof(float_t));
-   s.dir_mat[0] = 1;
-   s.dir_mat[4] = 1;
-   s.dir_mat[8] = 1;
-}
-
 void getAcelData() {                                                      // must be used only in the initialization state
   acel[0] = (Wire.read() << 8 | Wire.read())/16384.0;
   acel[1] = (Wire.read() << 8 | Wire.read())/16384.0;
@@ -81,45 +76,54 @@ void getAcelData() {                                                      // mus
 }
 
 float_t getInertialData(uint8_t c){                                       // stores the sensor data
-  acel[(c*3 + 0)] = (Wire.read() << 8 | Wire.read())/16384.0;
-  acel[(c*3 + 1)] = (Wire.read() << 8 | Wire.read())/16384.0;
-  acel[(c*3 + 2)] = (Wire.read() << 8 | Wire.read())/16384.0;
-  gyro[(c*3 + 0)] = (Wire.read() << 8 | Wire.read())/131 + 0.56;
-  gyro[(c*3 + 1)] = (Wire.read() << 8 | Wire.read())/131 - 2;
-  gyro[(c*3 + 2)] = (Wire.read() << 8 | Wire.read())/131 + 0.79;
-  time[c] = millis();
-  return (time[c]-time[!c])*0.001;                                        // returns the time elapsed from the last call in seconds
+  acel[0] = (Wire.read() << 8 | Wire.read())/16384.0;
+  acel[1] = (Wire.read() << 8 | Wire.read())/16384.0;
+  acel[2] = (Wire.read() << 8 | Wire.read())/16384.0;
+  gyro[0] = (Wire.read() << 8 | Wire.read())/131 + 0.56;
+  gyro[1] = (Wire.read() << 8 | Wire.read())/131 - 2;
+  gyro[2] = (Wire.read() << 8 | Wire.read())/131 + 0.79;
+  t[c] = millis();
+  return (t[c]-t[!c])*0.001;                                             // returns the time elapsed from the last call in seconds
 }
 
 void updateState() {
   curr = !curr;
   float_t dt = getInertialData(curr);
-  float_t rotation[3], drot[9], old_acel[3], avg_acel[3], rot_acel[3], trans_dir[9];
+  float_t rotation[3], drot[9], rot_acel[3], trans_dir[9];
 
-  rotation[0] = (((gyro[(curr*3 + 0)] + gyro[(!curr*3 + 0)])/2)*dt)*2*PI; // asume the gyroscope data to be changing at a constant rate in between 2 measurements
-  rotation[1] = (((gyro[(curr*3 + 1)] + gyro[(!curr*3 + 1)])/2)*dt)*2*PI;
-  rotation[2] = (((gyro[(curr*3 + 2)] + gyro[(!curr*3 + 2)])/2)*dt)*2*PI;
+  rotation[0] = (gyro[0]*dt)*2*PI;
+  rotation[1] = (gyro[1]*dt)*2*PI;
+  rotation[2] = (gyro[2]*dt)*2*PI;
 
   eul2Rotm(rotation, drot);                                               // calculating rotation matrix from old rotation matrix and gyroscope data
-  matMul(state[curr].dir_mat,drot,state[!curr].dir_mat);          
+  matMul(state[curr].dir_mat,drot,state[!curr].dir_mat);
 
-  matVecMul(old_acel, drot, *(acel[!curr*3]));                            // rotating acceleratoion of previous moment to current frame
-
-  avg_acel[0] = (acel[(curr*3 + 0)] + old_acel[0])/2;                     // asume the accelerometer data to be changing at a constant rate in between 2 measurements
-  avg_acel[1] = (acel[(curr*3 + 1)] + old_acel[1])/2;                                           
-  avg_acel[2] = (acel[(curr*3 + 2)] + old_acel[2])/2;                                           
   
   matTrans(trans_dir, state[curr].dir_mat);                               // transposing the rotation matrix from e-frame to b-frame to obtain the inverse from b-frame to e-frame
-  matVecMul(rot_acel, trans_dir, avg_acel);                               // rotating the avg acceleratoion to e-frame
+  matVecMul(rot_acel, trans_dir, acel);                                   // rotating the acceleratoion to e-frame
+
+  rot_acel[0] -= init_g[0];
+  rot_acel[1] -= init_g[1];
+  rot_acel[2] -= init_g[2];
 
   state[curr].vel[0] = state[!curr].vel[0] + rot_acel[0]*dt;              // calculating velocity and position
   state[curr].vel[1] = state[!curr].vel[1] + rot_acel[1]*dt;
   state[curr].vel[2] = state[!curr].vel[2] + rot_acel[2]*dt;
-  state[curr].pos[0] = state[!curr].pos[0] + ((state[curr].vel[0] + state[!curr].vel[0])/2)*dt;
-  state[curr].pos[1] = state[!curr].pos[1] + ((state[curr].vel[1] + state[!curr].vel[1])/2)*dt;
-  state[curr].pos[2] = state[!curr].pos[2] + ((state[curr].vel[2] + state[!curr].vel[2])/2)*dt;
+  state[curr].pos[0] = state[!curr].pos[0] + (state[curr].vel[0])*dt;
+  state[curr].pos[1] = state[!curr].pos[1] + (state[curr].vel[1])*dt;
+  state[curr].pos[2] = state[!curr].pos[2] + (state[curr].vel[2])*dt;
 };
 
+// touch Interrupt
+
+void gotTouchEvent(){
+  if (touch[!t_c] != touch[t_c]) {
+    touch[t_c] = !touch[t_c];
+    test_low = !test_low;
+    // Touch ISR will be inverted: Lower <--> Higher than the Threshold after ISR event is noticed
+    touchInterruptSetThresholdDirection(test_low);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -128,16 +132,12 @@ void setup() {
   BLEMidiServer.begin("MI.MU gloves dei poveri");
   Serial.println("Waiting for connections...");
   while(!BLEMidiServer.isConnected()) delay(100);
-  Serial.println("Bluetooth connected. \n Starting gravity estimation hold your hand still for 1 second.");
+  Serial.println("Bluetooth connected.");
 
-  stateInit(state[0]);
-  stateInit(state[1]);
+  touchAttachInterrupt(T2, gotTouchEvent, threshold);
+  touchInterruptSetThresholdDirection(test_low);
 
-  memset(0, acel, 6*sizeof(float_t));
-  memset(0, gyro, 6*sizeof(float_t));
-  memset(0, init_g, 3*sizeof(float_t));
-  memset(0, time, 2*sizeof(uint32_t));
-
+  Serial.print("Starting gravity estimation hold your hand still for 1 second.");
   float_t elapsed = 0;
   int16_t i = 0;
   while (elapsed < 1){                                                    // initializing the gravity vector by getting data for a second and averaging it
@@ -149,15 +149,25 @@ void setup() {
     i++;
     delay(10);
   }
-    init_g[0] /= i;
-    init_g[1] /= i;
-    init_g[2] /= i;
+  init_g[0] /= i;
+  init_g[1] /= i;
+  init_g[2] /= i;
+  t[0] = millis();
+  t[1] = t[0];
   Serial.print("Inital gravity calculation done. Used ");
   Serial.print(i);
   Serial.println(" accelerometer measurements");
 }
 
 void loop() {
+  if(touch[!c_t] != touch[c_t]){
+    if(touch[c_t]){
+      Serial.println("  ---- Touch was Pressed"); // note on and other stuff
+    } else {
+      Serial.println("  ---- Touch was Released"); // note off and other stuff
+    }
+    c_t = !c_t;
+  }
   if(BLEMidiServer.isConnected()) {     // If we've got a connection, we send an A4 during one second, at full velocity (127)
     Serial.println("connected");
       BLEMidiServer.noteOn(1, 69, 127);
